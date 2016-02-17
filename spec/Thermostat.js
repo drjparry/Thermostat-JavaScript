3834887a391eecb89137f0fb26a39e5b3955737f
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-02-17 14:56:07 +0100

    WIP - first expectation - Initial commit

---
+++ spec/Thermostat.js
@@ -0,0 +1,8 @@
+describe("Thermostat", function() {
+
+  it('starts at 20 degrees', function() {
+    var thermostat = new Thermostat();
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+});
