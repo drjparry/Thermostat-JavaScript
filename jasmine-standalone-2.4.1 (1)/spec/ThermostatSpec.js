9389803a93ee32672ca1a9a4280832a6bfee37eb
Finble <elainefinn@hotmail.com>
2016-02-18 12:24:39 +0000

    User Story 4 complete

--- jasmine-standalone-2.4.1 (1)/spec/ThermostatSpec.js
+++ jasmine-standalone-2.4.1 (1)/spec/ThermostatSpec.js
@@ -30,6 +30,18 @@ describe('Thermostat', function() {
 		expect(thermostat.getCurrentTemperature()).toEqual(19);
 	});
 
+//4. Thermostat has a minimum temperature is 10 degrees
+
+  // it('has a minimum temperature of 10 degrees', function() {
+  //   expect(thermostat.temperature).toEqual(20);
+  // });
+
+  it('has a minimum temperature of 10 degrees', function() {
+    for(var i = 0; i < 10; i ++) {
+      thermostat.decreaseTemperature();
+    }
+    expect(function(){
+      thermostat.decreaseTemperature()}).toThrowError('You cannot go below 10 degrees');
+  });
 });
 
-});
--- jasmine-standalone-2.4.1 (1)/src/Thermostat.js
+++ jasmine-standalone-2.4.1 (1)/src/Thermostat.js
@@ -1,7 +1,10 @@
 'use strict';
 
+//const minTemp = 10;
+
 function Thermostat(){
 	this.temperature = 20;
+	this.minTemp = 10;
 };
 
 Thermostat.prototype.getCurrentTemperature = function(){
@@ -13,9 +16,8 @@ Thermostat.prototype.increaseTemperature = function() {
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
-	this.temperature -- ; // -= 1
+	if(this.temperature <= this.minTemp) {
+		throw new Error('You cannot go below 10 degrees')};
+	this.temperature --; // -= 1
 };	
-
-
-
 
